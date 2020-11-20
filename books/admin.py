ed50c9d464873024aab39e4771abd3bd9b288c73
Feroz Khan <ferozkhan@pop-os.localdomain>
2020-11-20 11:00:54 +0500

    initial push

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import BookModel
 # Register your models here.
+
+from django_crud_app.admin_utils import set_admin
+
+set_admin(BookModel, ('book_name', 'author_name'), ('book_name', 'author_name'),
+          ('book_name', 'author_name', 'book_discription', 'publish_date'))
--- clients/admin.py
+++ clients/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import ClientUsers
+from django_crud_app.admin_utils import set_admin
 
 # Register your models here.
-admin.site.register(ClientUsers)
+# admin.site.register(ClientUsers)
+set_admin(ClientUsers, ('full_name', 'email', 'phone'), ('full_name', 'email', 'phone'),
+          ('full_name', 'email', 'phone', 'created_at'))
